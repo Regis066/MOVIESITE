64c4c6bfe3ec3542e476e17d6229f56cfb6809a3
Regis066 <mugisharegis72@gmail.com>
2023-05-08 11:27:22 +0200

    Deployment progress

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './index.html'
     })
   ]
 };
+
